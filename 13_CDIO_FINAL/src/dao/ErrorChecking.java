446c16fcda91b9e8722a88ff91a4a99bdd0e9fdd
thejs <thejsen@live.dk>
2018-05-31 14:26:18 +0200

    More error checking methods

--- 13_CDIO_FINAL/src/dao/ErrorChecking.java
+++ 13_CDIO_FINAL/src/dao/ErrorChecking.java
@@ -8,4 +8,20 @@ public class ErrorChecking {
 			return true;
 		}
 	}
+	
+	public static boolean checkIntSize(int input) {
+		if(input > 99999999 || input < 1){
+			return false;
+		}else {
+			return true;
+		}
+	}
+	
+	public static boolean checkStatus(int input) {
+		if(input < 0 || input > 2) {
+			return false;
+		}else {
+			return true;
+		}
+	}
 }
