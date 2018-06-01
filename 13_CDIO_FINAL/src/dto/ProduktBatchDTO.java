82b97dca1b2a2b33c3fe2327b36e982ba501e8da
Thyge S. Steffensen <s175176@student.dtu.dk>
2018-06-01 10:02:08 +0200

    DTO produkt... er opdateret #24

--- 13_CDIO_FINAL/src/dto/ProduktBatchDTO.java
+++ 13_CDIO_FINAL/src/dto/ProduktBatchDTO.java
@@ -1,45 +1,30 @@
 package dto;
 
-public class ProduktBatchDTO 
-{
-	int pbId;                     // i omraadet 1-99999999
-	int status;					// 0: ikke paabegyndt, 1: under produktion, 2: afsluttet
-	int receptId;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.extern.java.Log;
 
-	/**
-	 * Used for creating
-	 * @param status
-	 * @param receptId
-	 */
-	public ProduktBatchDTO(int status, int receptId)
-	{
-		this.status = status;
-		this.receptId = receptId;
-	}
-	/**
-	 * Used for updating
-	 * @param pbId
-	 * @param status
-	 * @param receptId
-	 */
-	public ProduktBatchDTO(int pbId, int status, int receptId)
-	{
-		this.pbId = pbId;
-		this.status = status;
-		this.receptId = receptId;
-	}
+@Log
+@Data
+@Builder
+@AllArgsConstructor
+public class ProduktBatchDTO {
+    int pbId;                     // i omraadet 1-99999999
+    int status;                    // 0: ikke paabegyndt, 1: under produktion, 2: afsluttet
+    int receptId;
 
-	public boolean isValid() {
-		return ErrorChecking.checkIntSize(pbId) && ErrorChecking.checkIntSize(status) && ErrorChecking.checkIntSize(receptId);
+    /**
+     * Used for creating
+     *
+     * @param status
+     * @param receptId
+     */
+    public ProduktBatchDTO(int status, int receptId) {
+        this.status = status;
+        this.receptId = receptId;
+    }
 
-	}
-
-	public int getPbId() { return pbId; }
-	public void setPbId(int pbId) { this.pbId = pbId; }
-	public int getStatus() { return status; }
-	public void setStatus(int status) { this.status = status; }
-	public int getReceptId() { return receptId; }
-	public void setReceptId(int receptId) { this.receptId = receptId; }
-	public String toString() { return pbId + "\t" + status + "\t" + receptId; }
 }
 
