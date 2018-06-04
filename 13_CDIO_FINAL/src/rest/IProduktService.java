1ab9aa108f7bc9a29b069152c46075d321812729
piilmann <christoffer@korn8.dk>
2018-06-04 11:32:27 +0200

    Rest interfaces defineret

--- 13_CDIO_FINAL/src/rest/IProduktService.java
+++ 13_CDIO_FINAL/src/rest/IProduktService.java
@@ -1,5 +1,23 @@
 package rest;
 
-public interface IProduktService {
+import java.util.List;
 
+import dto.DTOProduktBatch;
+import dto.DTOProduktBatchKomp;
+import exception.DALException;
+
+public interface IProduktService {
+	//Produkt batch
+	DTOProduktBatch getProduktBatch(int pbId) throws DALException; // Retuner produkt batch
+	List<DTOProduktBatch> getProduktBatchList() throws DALException; // Retuner en liste af alle produkt batch
+	void createProduktBatch(DTOProduktBatch produktbatch) throws DALException; //Opret et nyt produkt batch
+	void updateProduktBatch(DTOProduktBatch produktbatch) throws DALException; //Opdater et eksisterende produkt batch
+	void deleteProduktBatch(int pbID) throws DALException;
+	//Produkt batch komponent
+	DTOProduktBatchKomp getProduktBatchKomp(int pbId, int rbId) throws DALException; // Retuner et produkt batch komponent
+	List<DTOProduktBatchKomp> getProduktBatchKompList(int pbId) throws DALException;
+	List<DTOProduktBatchKomp> getProduktBatchKompList() throws DALException;
+	void createProduktBatchKomp(DTOProduktBatchKomp produktbatchkomponent) throws DALException;
+	void updateProduktBatchKomp(DTOProduktBatchKomp produktbatchkomponent) throws DALException;
+	void deleteProduktBatchKomp(int productBatch_ID, int raavareBatch_ID) throws DALException;
 }
--- 13_CDIO_FINAL/src/rest/IReceptService.java
+++ 13_CDIO_FINAL/src/rest/IReceptService.java
@@ -1,5 +1,23 @@
 package rest;
 
-public interface IReceptService {
+import java.util.List;
 
+import dto.DTORecept;
+import dto.DTOReceptKomp;
+import exception.DALException;
+
+public interface IReceptService {
+	//Recept
+	DTORecept getRecept(int receptId) throws DALException;
+	List<DTORecept> getReceptList() throws DALException;
+	void createRecept(DTORecept recept) throws DALException;
+	void updateRecept(DTORecept recept) throws DALException;
+	void deleteRecept(int input_id) throws DALException;
+	//Recept komponent
+	DTOReceptKomp getReceptKomp(int receptId, int raavareId) throws DALException;
+	List<DTOReceptKomp> getReceptKompList(int receptId) throws DALException;
+	List<DTOReceptKomp> getReceptKompList() throws DALException;
+	void createReceptKomp(DTOReceptKomp receptkomponent) throws DALException;
+	void updateReceptKomp(DTOReceptKomp receptkomponent) throws DALException;
+	void deleteReceptKomp(int recept_id, int raavare_id) throws DALException;
 }
