80c842a0cec0161b60866b4489ed1df172140a1c
christian <s164150@student.dtu.dk>
2018-06-05 09:02:38 +0200

    Styled button more.

--- 13_CDIO_FINAL/WebContent/recept.js
+++ 13_CDIO_FINAL/WebContent/recept.js
@@ -72,7 +72,7 @@ $(document).ready(function() {
 		return 	'<tr><th scope ="row">' + recept.receptId + '</th>' +
 		'<td><input type="text" id = "'+recept.receptId +'"class="form-control-plaintext" value="' + recept.receptNavn + '"></td></td>' +
 		'<td><button type="button" id = "'+recept.receptId+'"class="btn btn-primary vis" data-toggle="modal" data-target="#showMoreModal">▼</button>'+'</td>' +
-		'<td><button type="button" id = "'+recept.receptId+'"class="btn btn-primary slet" data-toggle="modal" data-target="#deleteModal">Slet</button>'+'</td>' +
+		'<td><button type="button" id = "'+recept.receptId+'"class="btn btn-primary slet" data-toggle="modal" data-target="#deleteModal">🗑🗑🗑🗑🗑🗑🗑🗑🗑Slet</button>'+'</td>' +
 		'</td></tr>';
 	}
 
