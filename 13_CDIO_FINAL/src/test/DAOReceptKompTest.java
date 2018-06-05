3c4b7a4e476cf6980ea5a0e81defce54a9d75a3d
Thyge S. Steffensen <s175176@student.dtu.dk>
2018-06-05 11:16:01 +0200

    ReceptKom testet #50

--- 13_CDIO_FINAL/src/test/DAOReceptKompTest.java
+++ 13_CDIO_FINAL/src/test/DAOReceptKompTest.java
@@ -18,11 +18,17 @@ class DAOReceptKompTest {
 
     @BeforeEach
     void setUp() {
-        try { new MySQLConnector(); }
-        catch (InstantiationException e) { e.printStackTrace(); }
-        catch (IllegalAccessException e) { e.printStackTrace(); }
-        catch (ClassNotFoundException e) { e.printStackTrace(); }
-        catch (SQLException e) { e.printStackTrace(); }
+        try {
+            new MySQLConnector();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
         receptkompDAO = new DAOReceptKomp();
     }
 
@@ -30,7 +36,7 @@ class DAOReceptKompTest {
     void getReceptKompPositiv() {
         try {
             Object actual;
-            receptKompDTO = receptkompDAO.getReceptKomp(1,1);
+            receptKompDTO = receptkompDAO.getReceptKomp(1, 1);
 
             actual = receptKompDTO.getReceptId();
             assertEquals(1, actual, "Recept ID was " + actual);
@@ -39,10 +45,10 @@ class DAOReceptKompTest {
             assertEquals(1, actual, "Raavare ID was " + actual);
 
             actual = receptKompDTO.getNomNetto();
-            assertEquals(10.0, actual, "NomNetto was " + actual);
+            assertEquals(1.0, actual, "NomNetto was " + actual);
 
             actual = receptKompDTO.getTolerance();
-            assertEquals(0.1, actual, "Tolerance was " + actual);
+            assertEquals(1.0, actual, "Tolerance was " + actual);
         } catch (DALException e) {
             System.out.println("Query could not be resolved.");
             e.printStackTrace();
@@ -57,7 +63,7 @@ class DAOReceptKompTest {
             receptKompDTO = receptkompDAO.getReceptKomp(receptID, raavareID);
             fail("Recept Komp existed");
         } catch (DALException e) {
-            if (!e.getMessage().contains("Recept komponenet med recept ID "+receptID+" og raavare ID "+raavareID+" findes ikke.")) {
+            if (!e.getMessage().contains("Recept komponenet med recept ID " + receptID + " og raavare ID " + raavareID + " findes ikke.")) {
                 fail(e.getMessage());
             }
         }
@@ -86,21 +92,27 @@ class DAOReceptKompTest {
 
     @Test
     void createReceptKomp() {
-        int lastid;
+        int lastid = 0;
         DTOReceptKomp compare;
         try {
-            lastid = receptkompDAO.getReceptKompList().size()-1;
+            lastid = receptkompDAO.getReceptKompList().size() - 1;
             lastid = receptkompDAO.getReceptKompList().get(lastid).getReceptId();
-            receptKompDTO = new DTOReceptKomp(lastid+1,1,10,0.1);
+            receptKompDTO = new DTOReceptKomp(lastid + 1, 1, 10, 0.1);
             receptkompDAO.createReceptKomp(receptKompDTO);
-            compare = receptkompDAO.getReceptKomp(receptKompDTO.getReceptId(),receptKompDTO.getRaavareId());
-            assertEquals(receptKompDTO.getReceptId(), compare.getReceptId(), "Recept ID was not "+ compare.getReceptId());
+            compare = receptkompDAO.getReceptKomp(receptKompDTO.getReceptId(), receptKompDTO.getRaavareId());
+            assertEquals(receptKompDTO.getReceptId(), compare.getReceptId(), "Recept ID was not " + compare.getReceptId());
             assertEquals(receptKompDTO.getRaavareId(), compare.getRaavareId(), "Raavare ID was not " + compare.getRaavareId());
             assertEquals(receptKompDTO.getNomNetto(), compare.getNomNetto(), "Nom Netto was not " + compare.getNomNetto());
             assertEquals(receptKompDTO.getTolerance(), compare.getTolerance(), "Tolerance was not " + compare.getTolerance());
         } catch (DALException e) {
             e.printStackTrace();
             fail("Could not create new Raavare");
+        } finally {
+            try {
+                MySQLConnector.doQuery("CALL deleteReceptKomp('" + (lastid + 1) + "','" + 1 + "');");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
     }
 
@@ -118,9 +130,9 @@ class DAOReceptKompTest {
             assertEquals(100.0, receptKompDTO.getNomNetto());
             receptKompDTO.setNomNetto(originalNetto);
             receptkompDAO.updateReceptKomp(receptKompDTO);
-        }catch (DALException e) {
+        } catch (DALException e) {
             fail("Invalid recept or raavare id: " + receptID + ", " + raavareID);
-        }catch (Exception e) {
+        } catch (Exception e) {
             fail("Something went wrong");
         }
     }
@@ -130,7 +142,7 @@ class DAOReceptKompTest {
         try {
             receptkompDAO.updateReceptKomp(new DTOReceptKomp(9999, 9999, 1, 1));
             fail("Batch existed.");
-        }catch (DALException e) {
+        } catch (DALException e) {
             if (!e.getMessage().contains("No rows updated in \"Recept komponent\"")) {
                 fail(e.getMessage());
             }
