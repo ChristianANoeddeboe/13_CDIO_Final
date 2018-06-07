74ca302bcc8b29298d5c19243b77eecb3fa77907
thejs <thejsen@live.dk>
2018-06-07 10:12:16 +0200

    Works now

--- 13_CDIO_FINAL/src/test/ReceptServiceTest.java
+++ 13_CDIO_FINAL/src/test/ReceptServiceTest.java
@@ -8,18 +8,41 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.JsonNode;
+import com.mashape.unirest.http.ObjectMapper;
 import com.mashape.unirest.http.Unirest;
 import com.mashape.unirest.http.exceptions.UnirestException;
+import com.mashape.unirest.http.options.Options;
 
 import dto.DTORecept;
 
 class ReceptServiceTest {
+	
 	String baseUrl = "http://207.154.253.254:8080/13_CDIO_FINAL/rest/recept/";
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
-		
+		Unirest.setObjectMapper(new ObjectMapper() {
+		    private com.fasterxml.jackson.databind.ObjectMapper jacksonObjectMapper
+		                = new com.fasterxml.jackson.databind.ObjectMapper();
+
+		    public <T> T readValue(String value, Class<T> valueType) {
+		        try {
+		            return jacksonObjectMapper.readValue(value, valueType);
+		        } catch (IOException e) {
+		            throw new RuntimeException(e);
+		        }
+		    }
+
+		    public String writeValue(Object value) {
+		        try {
+		            return jacksonObjectMapper.writeValueAsString(value);
+		        } catch (JsonProcessingException e) {
+		            throw new RuntimeException(e);
+		        }
+		    }
+		});
 	}
 
 	@AfterAll
@@ -31,9 +54,9 @@ class ReceptServiceTest {
 		
 		System.out.println("a");
 		try {
-			HttpResponse<JsonNode> response = Unirest.get(baseUrl+"all").asJson();
+			Options.refresh();			
 			System.out.println("a");
-			//HttpResponse<DTORecept[]> response = Unirest.get(baseUrl+"all").asObject(DTORecept[].class);
+			HttpResponse<DTORecept[]> response = Unirest.get(baseUrl+"all").asObject(DTORecept[].class);
 			System.out.println("a");
 		} catch (UnirestException e) {
 			// TODO Auto-generated catch block
