c09a6d6f3fe4998a9a9c8f53f35b9b2b02bc536f
Mathias Thejsen <thejsen@live.dk>
2018-06-12 07:50:36 +0200

    Removed unused imports

--- 13_CDIO_FINAL/src/ase/WeightSocket.java
+++ 13_CDIO_FINAL/src/ase/WeightSocket.java
@@ -7,7 +7,6 @@ import java.io.InputStreamReader;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
-import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
--- 13_CDIO_FINAL/src/test/DAORaavareTest.java
+++ 13_CDIO_FINAL/src/test/DAORaavareTest.java
@@ -8,7 +8,6 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.sql.SQLException;
-import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
