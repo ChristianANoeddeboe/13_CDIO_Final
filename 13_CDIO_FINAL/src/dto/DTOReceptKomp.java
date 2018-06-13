702e03a49e1d311d27fae3e400ee7b2be705e977
Thyge S. Steffensen <s175176@student.dtu.dk>
2018-06-13 08:22:22 +0200

    DTOOperatoer

--- 13_CDIO_FINAL/src/dto/DTOReceptKomp.java
+++ 13_CDIO_FINAL/src/dto/DTOReceptKomp.java
@@ -8,8 +8,8 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class DTOReceptKomp {
-    private int receptId;                  // auto genereres fra 1..n
+    private int receptId;              // auto genereres fra 1..n
     private int raavareId;             // i omraadet 1-99999999
-    private double nomNetto;            // skal vaere positiv og passende stor
-    private double tolerance;           // skal vaere positiv og passende stor
+    private double nomNetto;           // skal vaere positiv og passende stor
+    private double tolerance;          // skal vaere positiv og passende stor
 }
