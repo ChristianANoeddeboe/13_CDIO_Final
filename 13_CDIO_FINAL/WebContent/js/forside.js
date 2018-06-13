25241a4cc224105862017baa1cc67acb6fdbb7cd
christian <angercraft@gmail.com>
2018-06-13 13:54:22 +0200

    Tilføjede forside.

--- 13_CDIO_FINAL/WebContent/js/forside.js
+++ 13_CDIO_FINAL/WebContent/js/forside.js
@@ -1,25 +1,6 @@
 jQuery(document).ready(function() {
-
     var rolle;
-
-    $("#menuLoader").load("menu.html", null, function () {
-        rolle = localStorage.getItem('rolle');
-        $(".text-muted").text(rolle);
-        if (rolle === "Laborant") {
-            $("#operatoerAdmin").hide();
-            $("#receptAdmin").hide();
-            $("#raavareAdmin").hide();
-        }
-        if (rolle === "Værksfører") {
-            $("#receptAdmin").hide();
-            $("#operatoerAdmin").hide()
-        }
-        if (rolle === "Pharmaceut") {
-            $("#operatoerAdmin").hide();
-        }
-    });
-
-
-
-
+    loadMenu();
+    rolle = localStorage.getItem('rolle');
+    $(".text-muted").text(rolle);
 });
