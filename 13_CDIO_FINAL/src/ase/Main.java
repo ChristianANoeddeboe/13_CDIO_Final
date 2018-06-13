457a0f08586986164b1994a4360016e732a81184
Thyge S. Steffensen <s175176@student.dtu.dk>
2018-06-13 08:13:02 +0200

    Comments on Main->ASE

--- 13_CDIO_FINAL/src/ase/Main.java
+++ 13_CDIO_FINAL/src/ase/Main.java
@@ -1,11 +1,9 @@
 package ase;
 
-import connector.MySQLConnector;
-
 class Main{
 	public static void main (String[] args) {
-//		AseController ase = new AseController("169.254.2.2", 8000);
-		AseController ase = new AseController("62.79.16.17", 8000);
+//		AseController ase = new AseController("169.254.2.2", 8000);  // Connect over ethernet cable.
+		AseController ase = new AseController("62.79.16.17", 8000); // Connect over modem.
 		while(true){
             ase.run();
 		}
